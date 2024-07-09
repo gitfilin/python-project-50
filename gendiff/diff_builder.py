68291470cae3011e18c4c8878a88f1b7613260a0
vyfilin <vy.filin@yandex.ru>
2024-07-09 16:24:52 +0400

    fix building_diff

--- gendiff/diff_builder.py
+++ gendiff/diff_builder.py
@@ -1,7 +1,7 @@
 def building_diff(dict1, dict2):
     differences = {}
 
-    # Получаем добавленные и удаленные ключи
+    # возвращаем добавленные и удаленные ключи
     added_keys, removed_keys = get_keys_difference(dict1, dict2)
 
     all_keys = sorted(set(dict1.keys()).union(set(dict2.keys())))
@@ -11,22 +11,15 @@ def building_diff(dict1, dict2):
         value2 = dict2.get(key, "__UNDEFINED__")
 
         if key in added_keys:
-            differences[key] = {'status': 'added', 'value': value2}
+            adding_keys_diff(differences, key, 'added', value2)
         elif key in removed_keys:
-            differences[key] = {'status': 'removed', 'value': value1}
+            adding_keys_diff(differences, key, 'removed', value1)
         elif isinstance(value1, dict) and isinstance(value2, dict):
-            differences[key] = {'status': 'children',
-                                'diff': building_diff(value1, value2)}
+            differences[key] = adding_children(dict1, dict2, key)
         else:
-            if value1 == value2:
-                differences[key] = {'status': 'unchanged', 'value': value1}
-            elif value1 == "__UNDEFINED__":
-                differences[key] = {'status': 'added', 'value': value2}
-            elif value2 == "__UNDEFINED__":
-                differences[key] = {'status': 'removed', 'value': value1}
-            else:
-                differences[key] = {'status': 'changed',
-                                    'old': value1, 'new': value2}
+            diff_status = compare_values(value1, value2)
+            if diff_status:
+                differences[key] = diff_status
 
     return differences
 
@@ -37,3 +30,17 @@ def get_keys_difference(dict1, dict2):
     added_keys = keys2 - keys1
     removed_keys = keys1 - keys2
     return sorted(added_keys), sorted(removed_keys)
+
+
+def adding_keys_diff(differences, key, status, value):
+    differences[key] = {'status': status, 'value': value}
+
+
+def adding_children(dict1, dict2, key):
+    return {'status': 'children', 'diff': building_diff(dict1[key], dict2[key])}
+
+
+def compare_values(value1, value2):
+    if value1 == value2:
+        return {'status': 'unchanged', 'value': value1}
+    return {'status': 'changed', 'old': value1, 'new': value2}
